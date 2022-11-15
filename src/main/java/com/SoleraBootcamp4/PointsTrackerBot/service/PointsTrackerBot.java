5770f85dd025abed8080e83af0bb3ff8a0437a5c
Angel Urmente <urmenteangel@gmail.com>
2022-11-15 23:16:25 +0100

    Add Response support and close connection

--- src/main/java/com/SoleraBootcamp4/PointsTrackerBot/service/PointsTrackerBot.java
+++ src/main/java/com/SoleraBootcamp4/PointsTrackerBot/service/PointsTrackerBot.java
@@ -1,6 +1,8 @@
 package com.SoleraBootcamp4.PointsTrackerBot.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -67,10 +69,21 @@ public class PointsTrackerBot {
             con.setRequestProperty("Content-Type", "application/json");
             con.setDoOutput(true);
 
-            try(OutputStream os = con.getOutputStream()) {
+            try (OutputStream os = con.getOutputStream()) {
                 byte[] input = jsonStringified.getBytes("utf-8");
-                os.write(input, 0, input.length);			
+                os.write(input, 0, input.length);
             }
+
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+            }
+            con.disconnect();
 
         } catch (IOException e) {
             e.printStackTrace();
