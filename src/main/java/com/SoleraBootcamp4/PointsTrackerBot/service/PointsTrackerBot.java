20e9e0a94aef0843a6901d3f37824ce2e3e27194
Angel Urmente <urmenteangel@gmail.com>
2022-11-17 13:28:31 +0100

    Remove unused imports

--- src/main/java/com/SoleraBootcamp4/PointsTrackerBot/service/PointsTrackerBot.java
+++ src/main/java/com/SoleraBootcamp4/PointsTrackerBot/service/PointsTrackerBot.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Service;
 
 import com.SoleraBootcamp4.PointsTrackerBot.model.MessageChat;
